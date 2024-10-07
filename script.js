6d2f18ad2332adaf1d0ca124e0883fabe9afbe0d
bschelske <103594627+bschelske@users.noreply.github.com>
2024-10-06 22:25:17 -0500

    mobile friendly 😎

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+}
